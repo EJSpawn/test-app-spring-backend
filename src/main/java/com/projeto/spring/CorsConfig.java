341497989a34d570517ab39048057800a2972af0
Elvis Justino da SIlva <elvis.justino@hotmail.com>
2023-04-29 16:09:18 -0300

    fix

--- src/main/java/com/projeto/spring/CorsConfig.java
+++ src/main/java/com/projeto/spring/CorsConfig.java
@@ -4,15 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static org.springframework.security.config.Customizer.withDefaults;
-
-import java.util.List;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
@@ -23,17 +18,6 @@ public class CorsConfig implements WebMvcConfigurer {
         .allowedOrigins("*") // permite solicitações de qualquer origem
         .allowedMethods("GET", "POST", "PUT", "DELETE")
         .maxAge(3600);
-  }
-
-  @Bean
-  public CorsFilter corsFilter() {
-      final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-      final CorsConfiguration config = new CorsConfiguration();
-      config.setAllowedOrigins(List.of("*"));
-      config.setAllowedHeaders(List.of("Origin", "Content-Type", "Accept"));
-      config.setAllowedMethods(List.of("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
-      source.registerCorsConfiguration("/**", config);
-      return new CorsFilter(source);
   }
 
   @Bean
